3b827f515054495aa66c4ff1fb63f6a8141195a9
Maroz Dmitriy <dmitriy.maroz@intrahealth.com>
2021-03-08 10:46:53 +0300

    feat: task implement

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+ module.exports = function check(str, bracketsConfig) {
+    const config = new Map(bracketsConfig);
+    const strToArr = str.split('');
+    const arr = [];
+
+    for (let char of strToArr) {
+        if (arr.length && char === config.get(arr[arr.length - 1])) {
+            arr.pop();
+        } else if (config.has(char)) {
+            arr.push(char);
+        }
+        else if (char !== config.get(arr.pop())) {
+            return false;
+        }
+    }
+
+    return arr.length === 0;
 }
